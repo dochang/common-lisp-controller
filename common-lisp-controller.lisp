9898a0dc31fd88e74d5564055b786176a7b85a60
pvaneynd <pvaneynd@debian.org>
2006-10-02 13:52:33 +0200

    UNDO: reindent, remove *redirect-fasl-files-to-cache*
    darcs-hash:20061002115233-cca2d-2f072825be5fb9dfeb1d957d48648c99f0c6c8a4.gz

--- common-lisp-controller.lisp
+++ common-lisp-controller.lisp
@@ -17,34 +17,38 @@
 	   #:*clc-quiet*
 	   #:calculate-fasl-root
 	   #:list-systems
+	   #:*redirect-fasl-files-to-cache*
 	   ;; depricated:
 	   #:make-clc-send-command-string
 	   #:send-clc-command)
   (:nicknames #:clc
-					; depricated:
-  #:c-l-c))
+	      ; depricated:
+	      #:c-l-c))
 
 (in-package #:common-lisp-controller)
 
 (defvar *clc-quiet* nil
   "If true then clc prints no messages")
 
+(defvar *redirect-fasl-files-to-cache* nil
+  "If true we redirect the fasl files to the local cache directory")
+
 ;; Some general utilities to make the
 ;; descriptions shorter
 
 (defvar *fasl-type*
   (load-time-value
-  (pathname-type
-   (compile-file-pathname "foo.lisp")))
+   (pathname-type
+    (compile-file-pathname "foo.lisp")))
   "This is the type of compiled lisp files.")
 
 (defvar *fasl-root* nil "Root of implementation's directories of binary files")
 (defvar *source-root* #p"/usr/share/common-lisp/source/"
-  "Root of source directories")
+	"Root of source directories")
 (defvar *systems-root* #p"/usr/share/common-lisp/systems/"
-  "Root of systems directory")
+        "Root of systems directory")
 (defvar *image-preferences* #p"/etc/common-lisp/images/"
-  "Directory where user can choose what systems shall be added to
+	"Directory where user can choose what systems shall be added to
 Images per default")
 (defvar *implementation-name* nil "The name of the implementation,
 used to name the directory in /var/cache/common-lisp-controller")
@@ -54,24 +58,24 @@ used to name the directory in /var/cache/common-lisp-controller")
 (defun init-common-lisp-controller-v5 (implementation-name)
   ;; register the systems root:
   (setf *implementation-name* implementation-name)
-
+  
   (pushnew :common-lisp-controller *features*)
   (pushnew :clc-os-debian *features*)
 
   ;; put the central registry at the *end*
   ;; of the search list
   (appendf
-  (symbol-value (intern (symbol-name :*central-registry*)
-			(find-package :asdf)))
-  (list *systems-root*))
+   (symbol-value (intern (symbol-name :*central-registry*)
+			 (find-package :asdf)))
+   (list *systems-root*))
 
   ;; put the users asdf files at the FRONT
   ;; of the same search list
   (pushnew '(merge-pathnames ".clc/systems/"
 			     (user-homedir-pathname))
-  (symbol-value (intern (symbol-name :*central-registry*)
-			(find-package :asdf)))
-  :test #'equalp))
+	   (symbol-value (intern (symbol-name :*central-registry*)
+				 (find-package :asdf)))
+	   :test #'equalp))
 
 (defun compile-common-lisp-controller-v5 (implementation-name)
   "Compiles the clc files. Returns a list of fasls
@@ -87,104 +91,104 @@ that should be loaded in the list to enable clc"
 		      `(:relative "0" ,*implementation-name*))
 		     #p"/var/cache/common-lisp-controller/")))
 
-  (labels ((source-filename (package-name filename)
-			    (let ((file (parse-namestring filename)))
-			      (merge-pathnames
-			       (make-pathname
-				:name (pathname-name file)
-				:type (pathname-type file)
-				:directory (list :relative package-name))
-			       *source-root*)))
-	   #+ecl
-	   (system-fasl-filename (package-name filename)
-				 ;; this is complex because ecl
-				 ;; should produce system fasls,
-				 ;; and they have .o extension
-				 (merge-pathnames
-				  (make-pathname :type "o")
-				  (fasl-filename package-name filename)))
-	   (fasl-filename (package-name filename)
-			  ;; this is complex because ecl
-			  ;; should produce system fasls,
-			  ;; and they have .o extension
-			  (let* ((file (parse-namestring filename))
-				 (output-path
-				  (merge-pathnames
-				   (make-pathname :name (pathname-name file)
-						  :type (pathname-type file)
-						  :directory (list :relative package-name))
-				   fasl-root))
-				 (compiled-file-pathname
-				  (compile-file-pathname
-				   output-path)))
-			    compiled-file-pathname))
+    (labels ((source-filename (package-name filename)
+	       (let ((file (parse-namestring filename)))
+		 (merge-pathnames
+		  (make-pathname
+		   :name (pathname-name file)
+		   :type (pathname-type file)
+		   :directory (list :relative package-name))
+		  *source-root*)))
+	     #+ecl
+	     (system-fasl-filename (package-name filename)
+	       ;; this is complex because ecl
+	       ;; should produce system fasls,
+	       ;; and they have .o extension
+	       (merge-pathnames 
+		 (make-pathname :type "o")
+		 (fasl-filename package-name filename)))
+	     (fasl-filename (package-name filename)
+	       ;; this is complex because ecl
+	       ;; should produce system fasls,
+	       ;; and they have .o extension
+	       (let* ((file (parse-namestring filename))
+		      (output-path
+		       (merge-pathnames
+			(make-pathname :name (pathname-name file)
+				       :type (pathname-type file)
+				       :directory (list :relative package-name))
+			fasl-root))
+		      (compiled-file-pathname
+		       (compile-file-pathname
+			output-path)))
+		 compiled-file-pathname))
 
-	   (compile-and-load (package-name filename)
-			     (let* ((file-path (source-filename package-name filename))
-				    (compiled-file-pathname
-				     (progn
-				       ;; first make the target directory:
-				       (ensure-directories-exist
-					(fasl-filename package-name filename))
-				       ;; now compile it:
-				       (compile-file file-path
-						     :output-file (fasl-filename package-name filename)
-						     :print nil
-						     :verbose nil))))
-			       ;; then load it:
-			       (load compiled-file-pathname)
-			       ;; return fasl filename
-			       compiled-file-pathname
-			       ;; now for ecl: make the system file
-			       #+ecl
-			       (compile-file file-path
-					     :output-file
-					     (system-fasl-filename package-name filename)
-					     :print nil
-					     :verbose nil
-					     ;; make 'linkable object files'
-					     :system-p t))))
+	     (compile-and-load (package-name filename)
+	       (let* ((file-path (source-filename package-name filename))
+		      (compiled-file-pathname
+		       (progn
+			 ;; first make the target directory:
+			 (ensure-directories-exist
+			  (fasl-filename package-name filename))
+			 ;; now compile it:
+			 (compile-file file-path
+				       :output-file (fasl-filename package-name filename)
+				       :print nil
+				       :verbose nil))))
+		 ;; then load it:
+		 (load compiled-file-pathname)
+		 ;; return fasl filename
+		 compiled-file-pathname
+		 ;; now for ecl: make the system file
+		 #+ecl 
+		 (compile-file file-path
+			       :output-file
+			       (system-fasl-filename package-name filename)
+			       :print nil
+			       :verbose nil
+			       ;; make 'linkable object files' 
+			       :system-p t))))
 
-    ;; then asdf:
-    ;; For SBCL, take advantage of it's REQUIRE/contrib directories integration
-    #+sbcl
-    (when (boundp 'sb-ext::*module-provider-functions*)
-      (pushnew :sbcl-hooks-require cl:*features*))
-
-    ;; use the sbcl asdf version for increased
-    ;; compatibility with the normal sbcl
-
-    #+sbcl
-    (require :asdf)
-
-    ;; return a list
-    (prog1
-	(nconc
-	 (list
-
-	  ;; first ourselves:
-	  (compile-and-load  "common-lisp-controller"
-			     "common-lisp-controller.lisp")
-	  ;; asdf
-	  #-sbcl
-	  (compile-and-load  "asdf" "asdf.lisp")
-
-	  (compile-and-load  "asdf" "wild-modules.lisp")
-
-	  ;; now patch it::
-	  (compile-and-load "common-lisp-controller"
-			    "post-sysdef-install.lisp"))
-
-	 ;; so that it will neither recalculate it nor save it in our image
-	 (let ((*fasl-root* fasl-root))
-	   ;; "load-user-image-components" is in the above-loaded files.
-	   (funcall (symbol-function
-		     (find-symbol
-		      (symbol-name :load-user-image-components)
-		      :common-lisp-controller)))))
-
+      ;; then asdf:
+      ;; For SBCL, take advantage of it's REQUIRE/contrib directories integration
       #+sbcl
-      (setq cl:*features* (delete :sbcl-hooks-require  cl:*features*))))))
+      (when (boundp 'sb-ext::*module-provider-functions*)
+	(pushnew :sbcl-hooks-require cl:*features*))
+	
+      ;; use the sbcl asdf version for increased
+      ;; compatibility with the normal sbcl
+      
+      #+sbcl
+      (require :asdf)
+
+      ;; return a list
+      (prog1
+	  (nconc
+	   (list
+
+	    ;; first ourselves:
+	    (compile-and-load  "common-lisp-controller"
+			       "common-lisp-controller.lisp")
+	    ;; asdf
+	    #-sbcl
+	    (compile-and-load  "asdf" "asdf.lisp")
+	    
+	    (compile-and-load  "asdf" "wild-modules.lisp")
+	    
+	    ;; now patch it::
+	    (compile-and-load "common-lisp-controller"
+			      "post-sysdef-install.lisp"))
+
+	   ;; so that it will neither recalculate it nor save it in our image
+	   (let ((*fasl-root* fasl-root))
+	     ;; "load-user-image-components" is in the above-loaded files.
+	     (funcall (symbol-function
+		       (find-symbol
+			(symbol-name :load-user-image-components)
+			:common-lisp-controller)))))
+
+	#+sbcl
+	(setq cl:*features* (delete :sbcl-hooks-require  cl:*features*))))))
 
 (defun init-common-lisp-controller-v4 (implementation-name)
   "configures common-lisp-controller. IMPLEMENTATION-NAME
@@ -195,9 +199,9 @@ is the name of this implementation. Fasl's will be created in
   (init-common-lisp-controller-v5 implementation-name))
 
 (defun init-common-lisp-controller (fasl-root
-				    &key
-				    (source-root "/usr/share/common-lisp/")
-				    (version 2))
+                                    &key
+                                    (source-root "/usr/share/common-lisp/")
+                                    (version 2))
   (declare (ignore source-root version))
   ;; vodoo: extract the name of the implementation
   ;; from the old fasl directory...
--- post-sysdef-install.lisp
+++ post-sysdef-install.lisp
@@ -42,16 +42,16 @@
 #-(or cmu sbcl clisp allegro)
 (defun get-uid ()
   (labels ((mktemp ()
-		   (let ((temp-name (format nil "/tmp/clc-~A" (random 50000))))
-		     (with-open-file (file temp-name :direction :input :if-does-not-exist nil)
-				     (if file (mktemp) temp-name)))))
+	     (let ((temp-name (format nil "/tmp/clc-~A" (random 50000))))
+	       (with-open-file (file temp-name :direction :input :if-does-not-exist nil)
+		 (if file (mktemp) temp-name)))))
 
     (let ((file (mktemp)))
       (unwind-protect
-	  (progn (asdf:run-shell-command "umask 077 && echo $UID > ~A" file)
-		 (with-open-file (uid file :direction :input)
-				 (handler-case (parse-integer (read-line uid))
-					       (error () (error "Unable to find out user ID")))))
+	   (progn (asdf:run-shell-command "umask 077 && echo $UID > ~A" file)
+		  (with-open-file (uid file :direction :input)
+		    (handler-case (parse-integer (read-line uid))
+		      (error () (error "Unable to find out user ID")))))
 	(asdf:run-shell-command "rm ~A" file)))))
 
 #+clisp
@@ -97,29 +97,29 @@
 (defmacro with-secure-umask (&body forms)
   (let ((old-umask (gensym)))
     `(let ((,old-umask (sb-posix:umask #o0077)))
-       (unwind-protect (progn ,@forms)
-	 (sb-posix:umask ,old-umask)))))
+      (unwind-protect (progn ,@forms)
+	(sb-posix:umask ,old-umask)))))
 
 #+clisp
 (defmacro with-secure-umask (&body forms)
   (let ((old-umask (gensym)))
     `(let ((,old-umask (posix:umask #o077)))
-       (unwind-protect ,@forms
-	 (posix:umask ,old-umask)))))
+      (unwind-protect ,@forms
+	(posix:umask ,old-umask)))))
 
 #+cmu
 (defmacro with-secure-umask (&body forms)
   (let ((old-umask (gensym)))
     `(let ((,old-umask (unix::unix-umask #o077)))
-       (unwind-protect ,@forms
-	 (unix::unix-umask ,old-umask)))))
+      (unwind-protect ,@forms
+	(unix::unix-umask ,old-umask)))))
 
 #+allegro
 (defmacro with-secure-umask (&body forms)
   (let ((old-umask (gensym)))
     `(let ((,old-umask (excl.osi:umask #o077)))
-       (unwind-protect ,@forms
-	 (excl.osi:umask old-umask)))))
+      (unwind-protect ,@forms
+	(excl.osi:umask old-umask)))))
 
 #+(or clisp sbcl cmu allegro)
 (defun check-spooldir-security (target)
@@ -138,7 +138,7 @@
 	  (when (group-writable? mode)
 	    (error "Security problem: the cache directory ~S is writable for a group, for better security we do not allow this"
 		   target)))
-      (with-secure-umask (ensure-directories-exist target))))
+	(with-secure-umask (ensure-directories-exist target))))
   (values))
 
 ;; sucks but is portable ;-(
@@ -170,7 +170,7 @@ if (($stat->mode & 020) != 0) {
    exit 46;
 }
 exit 0;' 2>&1 3>&1"
-				 target)))
+				     target)))
     (case result
       (0 nil)
       (42 (error "Security problem: Could not stat ~A" target))
@@ -183,18 +183,18 @@ exit 0;' 2>&1 3>&1"
 (defun calculate-fasl-root  ()
   "Inits common-lisp controller for this user"
   (or *fasl-root*
-      (setf *fasl-root*
-	    (let ((target-root (merge-pathnames
-				(make-pathname
-				 :directory (list :relative (format nil "~A" (get-uid))))
-				#p"/var/cache/common-lisp-controller/")))
-	      (check-spooldir-security target-root)
-	      (merge-pathnames (make-pathname
-				:directory (list :relative *implementation-name*))
-			       target-root)))))
+    (setf *fasl-root*
+	  (let ((target-root (merge-pathnames
+			      (make-pathname
+			       :directory (list :relative (format nil "~A" (get-uid))))
+			      #p"/var/cache/common-lisp-controller/")))
+	    (check-spooldir-security target-root)
+	    (merge-pathnames (make-pathname
+			      :directory (list :relative *implementation-name*))
+			     target-root)))))
 
 (defun asdf-system-compiled-p (system)
-  "Returns T is an ASDF system is already compiled"
+  "Returns T is an ASDF system is already compiled" 
   (notany #'(lambda (op) (and (typep (car op) 'asdf:compile-op)
 			      (not (asdf:operation-done-p (car op) (cdr op)))))
 	  (asdf::traverse (make-instance 'asdf:compile-op) system)))
@@ -232,46 +232,49 @@ exit 0;' 2>&1 3>&1"
 				      :while root
 				      :unless (equal root (first tail)) :do
 				      (error "Path ~S not beneath ~S? ~S /= ~S"
-					     source source-root root (first tail))
+					  source source-root root (first tail))
 				      :finally (return tail)))
 	       :defaults source))))
-    (merge-pathnames  relative-source *fasl-root*)))
+  (merge-pathnames  relative-source *fasl-root*)))
 
 (defun alternative-root-path-to-fasl-path (source)
   "Converts a path in anywhere to a path beneath the fasl root"
   (let ((source-path (pathname-directory source
 					 :case :local)))
     (cond
-     ;; it could already be beneath /var/cache/common-lisp
-     ((and (eq (first source-path) :ABSOLUTE)
-	   (eq (second source-path) "var")
-	   (eq (third source-path) "cache")
-	   (eq (fourth source-path) "common-lisp"))
-      ;; just let it be
-      source)
-     ;; it could be a library package
-     ((and (eq (first source-path) :ABSOLUTE)
-	   (eq (second source-path) "usr")
-	   (eq (third source-path) "lib"))
-      ;; just let it be
-      source)
-     (t
-      (merge-pathnames
-       (make-pathname :directory
-		      (append (list :RELATIVE "local")
-			      (rest source-path))
-		      :case :local
-		      :defaults source)
-       *fasl-root*)))))
+      ;; it could already be beneath /var/cache/common-lisp
+      ((and (eq (first source-path) :ABSOLUTE)
+	    (eq (second source-path) "var")
+	    (eq (third source-path) "cache")
+	    (eq (fourth source-path) "common-lisp"))
+       ;; just let it be
+       source)
+      ;; it could be a library package
+      ((and (eq (first source-path) :ABSOLUTE)
+	    (eq (second source-path) "usr")
+	    (eq (third source-path) "lib"))
+       ;; just let it be
+       source)
+      (t
+       (merge-pathnames
+	(make-pathname :directory
+		       (append (list :RELATIVE "local")
+			       (rest source-path))
+		       :case :local
+		       :defaults source)
+	*fasl-root*)))))
 
 (defmethod asdf:output-files :around ((op asdf:operation) (c asdf:component))
   "Method to rewrite output files to fasl-root"
   (let ((orig (call-next-method)))
-    (calculate-fasl-root)
-    (cond ((beneath-source-root? c)
-	   (mapcar #'source-root-path-to-fasl-path orig))
-	  (t
-	   (mapcar #'alternative-root-path-to-fasl-path orig)))))
+    (if (not *redirect-fasl-files-to-cache*)
+	orig
+      (progn
+	(calculate-fasl-root)
+	(cond ((beneath-source-root? c)
+	       (mapcar #'source-root-path-to-fasl-path orig))
+	      (t
+	       (mapcar #'alternative-root-path-to-fasl-path orig)))))))
 
 (defun system-in-source-root? (c)
   "Returns T if component's directory is the same as *source-root* + component's name"
@@ -295,26 +298,27 @@ exit 0;' 2>&1 3>&1"
   (let ((system (asdf:find-system module-name)))
     (when system
       (if (asdf-system-compiled-p system)
-	  (asdf:oos 'asdf:load-op module-name)
-	(progn
-	  (unless *clc-quiet*
-	    (format t "~&;;; Please wait, recompiling library..."))
-	  (asdf:oos 'asdf:compile-op module-name)
-	  (terpri)
-	  (asdf:oos 'asdf:load-op module-name)))
+		  (asdf:oos 'asdf:load-op module-name)
+		(progn
+		  (unless *clc-quiet*
+		    (format t "~&;;; Please wait, recompiling library..."))
+		  (asdf:oos 'asdf:compile-op module-name)
+		  (terpri)
+		  (asdf:oos 'asdf:load-op module-name)))
       t)))
 
 (defun clc-require (module-name &optional (pathname 'clc::unspecified))
-  (if (not (eq pathname 'clc::unspecified))
-      (common-lisp:require module-name pathname)
-    (let ((system-type (find-system-def module-name)))
-      (case system-type
-	(:asdf
-	 (require-asdf module-name))
-	;; Don't call original-require with SBCL since we are called by that function
-	#-sbcl
-	(otherwise
-	 (common-lisp:require module-name))))))
+  (let ((*redirect-fasl-files-to-cache* t))
+    (if (not (eq pathname 'clc::unspecified))
+	(common-lisp:require module-name pathname)
+      (let ((system-type (find-system-def module-name)))
+	(case system-type
+	  (:asdf
+	   (require-asdf module-name))
+	  ;; Don't call original-require with SBCL since we are called by that function
+	  #-sbcl 
+	  (otherwise
+	   (common-lisp:require module-name)))))))
 
 
 (defun clc-build-all-packages (&optional (ignore-errors nil))
@@ -329,13 +333,14 @@ If IGNORE-ERRORS is true ignores all errors while rebuilding"
 			   failed-packages))
 	:do
 	(loop :for pathname :in (directory
-				 (merge-pathnames
-				  (make-pathname :name :wild :type "asd")
+			         (merge-pathnames
+			      	  (make-pathname :name :wild :type "asd")
 				  registry-location))
 	      :for package-name = (pathname-name pathname) :do
 	      (restart-case
-	       (handler-case
-		(asdf:oos 'asdf:compile-op package-name)
+	       (handler-case 
+		(let ((*redirect-fasl-files-to-cache* t))
+		  (asdf:oos 'asdf:compile-op package-name))
 		(error (error)
 		       (cond (ignore-errors
 			       (format t "~&Ignoring error: ~A~%" error)
@@ -362,7 +367,7 @@ If IGNORE-ERRORS is true ignores all errors while rebuilding"
 		(setf (gethash system systems) system)))
     (format t
 	    "~&Known systems:~%~@<~;:~A~_ ~;~:>~%"
-	    (sort
+	    (sort 
 	     (loop :for system :being :the :hash-keys :of systems
 		   :collect system)
 	     #'string<))
@@ -385,11 +390,11 @@ If IGNORE-ERRORS is true ignores all errors while rebuilding"
 			       *image-preferences*
 			       *implementation-name*)
 			      :direction :input :if-does-not-exist nil)
-		  (when components
-		    (let ((asdf:*central-registry*
-			   (append asdf:*central-registry* (list *systems-root*))))
-		      (loop for component = (read-line components nil)
-			    while component nconc
-			    (let ((system (asdf:find-system component nil)))
-			      (if system (load-component system)
-				(warn "System ~S not found, not loading it into implementation image" component))))))))
+    (when components
+      (let ((asdf:*central-registry*
+	     (append asdf:*central-registry* (list *systems-root*))))
+	(loop for component = (read-line components nil)
+	      while component nconc
+	      (let ((system (asdf:find-system component nil)))
+		(if system (load-component system)
+		    (warn "System ~S not found, not loading it into implementation image" component))))))))
